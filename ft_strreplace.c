813fe4b743e634e411ebb9081cdda4acc8b18ed7
Zack BATIK <zbatik@e4r21p4.wethinkcode.co.za>
2018-06-22 17:38:57 +0200

    fixed norm error in replace

--- ft_strreplace.c
+++ ft_strreplace.c
@@ -6,14 +6,14 @@
 /*   By: zbatik <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/21 17:14:16 by zbatik            #+#    #+#             */
-/*   Updated: 2018/06/21 17:40:01 by zbatik           ###   ########.fr       */
+/*   Updated: 2018/06/22 17:38:12 by zbatik           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include "libft.h"
 
-char	*ft_strreplace(char **replace, char * with)
+char	*ft_strreplace(char **replace, char *with)
 {
 	if (replace == NULL || with == NULL)
 		return (NULL);
