15e5ceda4d0629ac3ffb432054b4aaba7ab85ac3
Zack BATIK <zbatik@e4r21p4.wethinkcode.co.za>
2018-07-10 17:24:25 +0200

    end of day

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: zbatik <marvin@42.fr>                      +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2018/05/24 11:33:49 by zbatik            #+#    #+#              #
-#    Updated: 2018/07/10 12:08:30 by zbatik           ###   ########.fr        #
+#    Updated: 2018/07/10 12:14:09 by zbatik           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -87,7 +87,7 @@ FILES		= 	ft_atoi \
 				ft_strcdrop \
 				ft_strreplace \
 				get_next_line \
-				skip_lines \	
+				skip_lines	
 
 CFILES		= $(addsuffix .c, $(FILES))
 OFILES		= $(addsuffix .o, $(FILES))
--- skip_lines.c
+++ skip_lines.c
@@ -6,26 +6,29 @@
 /*   By: zbatik <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/10 12:02:32 by zbatik            #+#    #+#             */
-/*   Updated: 2018/07/10 12:11:39 by zbatik           ###   ########.fr       */
+/*   Updated: 2018/07/10 12:21:19 by zbatik           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
 #include "libft.h"
 
 int	skip_lines(const int fd, int skip)
 {
 	int		i;
+	int		ret;
 	char	*line;
 
 	if (skip < 1)
 		return (-1);
 	i = 0;
 	ret = 1;
-	while (i++ < skip && ret < 0)
+	while (i < skip && ret > 0)
 	{
 		ret = get_next_line(fd, &line);
 		if (line != NULL)
 			free(line);
+		i++;
 	}
 	return (ret);
 }
