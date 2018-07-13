e08152646df8ad58668451636c5c0fe012337a6f
Zack BATIK <zbatik@e4r21p4.wethinkcode.co.za>
2018-07-13 12:07:53 +0200

    fixed arrnew

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: zbatik <marvin@42.fr>                      +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2018/05/24 11:33:49 by zbatik            #+#    #+#              #
-#    Updated: 2018/07/13 11:58:37 by zbatik           ###   ########.fr        #
+#    Updated: 2018/07/13 12:06:18 by zbatik           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -88,7 +88,7 @@ FILES		= 	ft_atoi \
 				ft_strreplace \
 				get_next_line \
 				skip_lines \
-				arrnew 	
+				ft_arrnew 	
 
 CFILES		= $(addsuffix .c, $(FILES))
 OFILES		= $(addsuffix .o, $(FILES))
--- ft_arrnew.c
+++ ft_arrnew.c
@@ -6,7 +6,7 @@
 /*   By: zbatik <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/13 11:54:42 by zbatik            #+#    #+#             */
-/*   Updated: 2018/07/13 11:57:52 by zbatik           ###   ########.fr       */
+/*   Updated: 2018/07/13 12:07:20 by zbatik           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ char	**ft_arrnew(size_t x, size_t y)
 	char	**arr;
 	size_t	i;
 
-	arr = (char**)malloc(sizeof(char*) * (x + 1));
+	arr = (char**)ft_memalloc(sizeof(char*) * (x + 1));
 	i = 0;
 	while (i < x)
 		arr[i++] = ft_strnew(y);
