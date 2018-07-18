78cf708652deda66c89bc6f6bc3bfc23695f2354
Zack BATIK <zbatik@e4r21p4.wethinkcode.co.za>
2018-07-18 15:14:48 +0200

    gnl BUFF_SZIE 1

--- includes/get_next_line.h
+++ includes/get_next_line.h
@@ -6,14 +6,14 @@
 /*   By: zbatik <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/04 13:31:47 by zbatik            #+#    #+#             */
-/*   Updated: 2018/07/10 12:09:17 by zbatik           ###   ########.fr       */
+/*   Updated: 2018/07/18 15:13:32 by zbatik           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# define BUFF_SIZE 32
+# define BUFF_SIZE 1
 
 # include "libft.h"
 
