3645dd0384e7f7fb6d9b02508940dee604a808bb
Event WeThinkCode <event@c3r7s6.wethinkcode.co.za>
2018-08-17 17:03:11 +0200

    fixed bug on reset with na colour

--- put/ft_putendl_cl.c
+++ put/ft_putendl_cl.c
@@ -6,7 +6,7 @@
 /*   By: zbatik <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/27 12:11:05 by zbatik            #+#    #+#             */
-/*   Updated: 2018/07/27 16:29:23 by zbatik           ###   ########.fr       */
+/*   Updated: 2018/08/17 17:01:54 by event            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,5 @@ void	ft_putendl_cl(char *s, t_colour colour)
 {
 	ft_putstr(select_colour(colour));
 	ft_putstr(s);
-	ft_putendl(ANSI_COLOUR_RESET);
+	ft_putendl(SWTCH(colour));
 }
